81edbbeb3d215a151bf2b1f8a5a7c7f45e1cc9dc
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-17 12:18:56 +0100

    Rerun suitable for a run of longer than one hour [skip ci]

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -7,6 +7,15 @@
 #   ./scripts/rerun.sh
 #
 #
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --job-name=pirouette_example_28
+#SBATCH --output=pirouette_example_28.log
+module load R
+
 rm -rf example_28_314
 time Rscript example_28.R
 
